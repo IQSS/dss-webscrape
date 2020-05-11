88e254cf04b4fde2060ed7c3ded5898be79cbd09
liujinjie111 <jinjie_liu@fas.harvard.edu>
2020-05-11 00:45:30 -0400

    publish part_1

--- build.ps1
+++ build.ps1
@@ -1,12 +1,12 @@
 Write-Host "EBook creation starts"
  
-& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('rmarkdown', repos='http://cran.case.edu/')"
-& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('bookdown', repos='http://cran.case.edu/')"
-& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('reticulate', repos='http://cran.case.edu/')"
+& "C:\Program Files\R\R-3.6.2\bin\rscript.exe" -e "install.packages('rmarkdown', repos='http://cran.case.edu/')"
+& "C:\Program Files\R\R-3.6.2\bin\rscript.exe" -e "install.packages('bookdown', repos='http://cran.case.edu/')"
+& "C:\Program Files\R\R-3.6.2\bin\rscript.exe" -e "install.packages('reticulate', repos='http://cran.case.edu/')"
  
-& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "bookdown::render_book('part_1.rmd', 'bookdown::gitbook')"
-#& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "bookdown::render_book('rmd_files', 'bookdown::pdf_book')"
-#& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "bookdown::render_book('rmd_files', 'bookdown::epub_book')"
+& "C:\Program Files\R\R-3.6.2\bin\rscript.exe" -e "bookdown::render_book('part_1.rmd', 'bookdown::gitbook')"
+#& "C:\Program Files\R\R-3.6.2\bin\rscript.exe" -e "bookdown::render_book('rmd_files', 'bookdown::pdf_book')"
+#& "C:\Program Files\R\R-3.6.2\bin\rscript.exe" -e "bookdown::render_book('rmd_files', 'bookdown::epub_book')"
 
 & copy-item .nojekyll docs\ 
 & Get-ChildItem *.rds,*.log -recurse | Remove-Item
